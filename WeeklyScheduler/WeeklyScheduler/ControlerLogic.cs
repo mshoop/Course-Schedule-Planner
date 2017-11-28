dfe01c285a40b36e2acd60c6d555bdccef68e973
mshoop <maxwell.shoop@gmail.com>
2017-11-28 14:59:32 -0700

    Initial commit

---
+++ WeeklyScheduler/WeeklyScheduler/ControlerLogic.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace WeeklyScheduler
+{
+    public partial class MainWindow : Window
+    {
+
+        public class Section
+        {
+            public readonly int sectionNumber;
+            public readonly int startTime;
+            public readonly Boolean MoWeFr;
+
+            public Section(int startTime, int sectionNumber, Boolean MoWeFr)
+            {
+                this.startTime = startTime;
+                this.sectionNumber = sectionNumber;
+                this.MoWeFr = MoWeFr;
+            }
+
+            public override bool Equals(object obj)
+            {
+                // If parameter is null return false.
+                if (obj == null)
+                {
+                    return false;
+                }
+
+                // If parameter cannot be cast to Point return false.
+                Section p = obj as Section;
+                if ((System.Object)p == null)
+                {
+                    return false;
+                }
+
+                // Return true if the fields match:
+                return sectionNumber == p.sectionNumber && startTime == p.startTime && MoWeFr == p.MoWeFr;
+            }
+
+            public override int GetHashCode()
+            {
+                return sectionNumber;
+            }
+        }
+
+        public class Class
+        {
+            public readonly String courseNumber;
+            public readonly ArrayList listOfSections;
+
+            public Class(String courseNumber, ArrayList listOfSections)
+            {
+                this.courseNumber = courseNumber;
+                this.listOfSections = ArrayList.ReadOnly(listOfSections);
+            }
+
+            public override bool Equals(object obj)
+            {
+                // If parameter is null return false.
+                if (obj == null)
+                {
+                    return false;
+                }
+
+                // If parameter cannot be cast to Point return false.
+                Class p = obj as Class;
+                if ((System.Object)p == null)
+                {
+                    return false;
+                }
+
+                // Return true if the fields match:
+                return courseNumber.Equals(p.courseNumber);
+            }
+
+            public override int GetHashCode()
+            {
+                return courseNumber.GetHashCode();
+            }
+
+        }
+
+        //Member Variables
+        public ArrayList listOfClasses;
+        public List<KeyValuePair<Class, Section>>[,] schedule = new List<KeyValuePair<Class, Section>>[5, 25];
+
+        //Constructor
+        public void IntItData()
+        {
+            ArrayList classes = new ArrayList();
+
+            //Populate classes/sections
+            ArrayList sections = new ArrayList();
+
+            sections.Add(new Section(9, 1, true));
+            sections.Add(new Section(14, 2, true));
+            sections.Add(new Section(8, 3, true));
+            classes.Add(new Class("MATH 216", new ArrayList(sections)));
+            sections.Clear();
+
+            sections.Add(new Section(13, 1, true));
+            sections.Add(new Section(14, 2, true));
+            sections.Add(new Section(12, 3, false));
+            classes.Add(new Class("PHYS 201", new ArrayList(sections)));
+            sections.Clear();
+
+            sections.Add(new Section(13, 1, false));
+            sections.Add(new Section(14, 3, false));
+            classes.Add(new Class("CptS 423", new ArrayList(sections)));
+            sections.Clear();
+
+            sections.Add(new Section(9, 1, true));
+            sections.Add(new Section(13, 2, true));
+            sections.Add(new Section(10, 3, false));
+            classes.Add(new Class("MATH 172", new ArrayList(sections)));
+            sections.Clear();
+
+            sections.Add(new Section(10, 1, true));
+            sections.Add(new Section(12, 2, true));
+            sections.Add(new Section(11, 3, false));
+            classes.Add(new Class("CptS 260", new ArrayList(sections)));
+            sections.Clear();
+
+            listOfClasses = ArrayList.ReadOnly(classes);
+
+
+            for (int i = 0; i < 25; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    schedule[j, i] = new List<KeyValuePair<Class, Section>>();
+                }
+            }
+        }
+
+        //Adds a given section of a given class to the digital schedule
+        public void AddSection(Class cla, Section sect)
+        {
+            KeyValuePair<Class, Section> toInsert = new KeyValuePair<Class, Section>(cla, sect);
+            if (sect.MoWeFr)
+            {
+                schedule[0, sect.startTime].Add(toInsert);
+                schedule[2, sect.startTime].Add(toInsert);
+                schedule[4, sect.startTime].Add(toInsert);
+            }
+            else
+            {
+                schedule[1, sect.startTime].Add(toInsert);
+                schedule[3, sect.startTime].Add(toInsert);
+            }
+            UpdateSchedule();
+        }
+
+        //Removes a given section of a given class from the digital schedule
+        public void RemoveSection(Class cla, Section sect)
+        {
+            KeyValuePair<Class, Section> toRemove = new KeyValuePair<Class, Section>(cla, sect);
+            if (sect.MoWeFr)
+            {
+                schedule[0, sect.startTime].Remove(toRemove);
+                schedule[2, sect.startTime].Remove(toRemove);
+                schedule[4, sect.startTime].Remove(toRemove);
+            }
+            else
+            {
+                schedule[1, sect.startTime].Remove(toRemove);
+                schedule[3, sect.startTime].Remove(toRemove);
+            }
+            UpdateSchedule();
+        }
+
+        public void RemoveClass(Class cla)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    for (int k = 0; k < schedule[j, i].Count; k++)
+                    {
+                        if (schedule[j, i][k].Key.Equals(cla))
+                        {
+                            schedule[j, i].RemoveAt(k);
+                            k--;
+                        }
+                    }
+
+                }
+            }
+            UpdateSchedule();
+        }
+
+        //Called when ever the digital schedule has been updated in order to update the visual schedule.
+        public void UpdateSchedule()
+        {
+            Boolean popedup = false;
+            scheduleDataGrid.Items.Clear();
+            //Rebuild the UI of the schedule as necessary to reflect what is in the data.
+
+            //Go through each index that maps to a cell in the UI
+            //If empty clear said cell
+            //If occupied mark cell as green, fill with text, mark matching drop down box as green.
+            //If over-occupied mark as red, fill with both texts?, mark matching drop down boxes as green, grey out 'finish'.
+            for (int time = 7; time < 19; time++)
+            {
+                DaysOfWeek row = new DaysOfWeek();
+                for (int day = 0; day < 5; day++)//Loop through each timeslot in the schedule.
+                {
+                    List<KeyValuePair<Class, Section>> timeSlot = schedule[day, time];
+                    String daystring = "";
+                    if (timeSlot.Count == 0)
+                    {
+                        daystring = "";
+                    }
+
+                    // One class at this time
+                    else if (timeSlot.Count == 1)
+                    {
+                        daystring = schedule[day, time][0].Key.courseNumber + "\n" + schedule[day, time][0].Value.startTime + ":00-" + (schedule[day, time][0].Value.startTime + 1) + ":00 ";
+                    }
+
+                    // Overbooked
+                    else if (timeSlot.Count > 1)
+                    {
+                        StringBuilder balh = new StringBuilder("");
+
+                        foreach(KeyValuePair<Class,Section> cla in schedule[day, time])
+                        {
+                            balh.Append(cla.Key.courseNumber +"\n"+ schedule[day, time][0].Value.startTime + ":00-" + (schedule[day, time][0].Value.startTime + 1) + ":00 " + "\n");
+                        }
+
+                        daystring = balh.ToString();
+                        if(!popedup)
+                            MessageBox.Show("Some of your classes are conflicting!");
+                        popedup = true;
+
+                    }
+
+                    switch (day)
+                    {
+                        case 0: row.monday = daystring; break;
+                        case 1: row.tuesday = daystring; break;
+                        case 2: row.wednesday = daystring; break;
+                        case 3: row.thursday = daystring; break;
+                        case 4: row.friday = daystring; break;
+                    }
+                }
+                scheduleDataGrid.Items.Add(row);
+            }
+        }
+    }
+}
---
+++ WeeklyScheduler/WeeklyScheduler/MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace WeeklyScheduler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            addRowsToDataGrid();
+            IntItData();
+        }
+
+        public class DaysOfWeek
+        {
+            public string monday { get; set; }
+            public string tuesday { get; set; }
+            public string wednesday { get; set; }
+            public string thursday { get; set; }
+            public string friday { get; set; }
+        }
+
+        private void addRowsToDataGrid()
+        {
+        }
+
+        private void undoButton_Click(object sender, RoutedEventArgs e)
+        {
+            // undo not implemented
+        }
+
+        private void redoButton_Click(object sender, RoutedEventArgs e)
+        {
+            // redo not implemented
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            class1Menu.SelectedIndex = 0;
+            class2Menu.SelectedIndex = 0;
+            class3Menu.SelectedIndex = 0;
+            class4Menu.SelectedIndex = 0;
+            class5Menu.SelectedIndex = 0;
+        }
+
+        private void showOnlyOpenSectionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // probably don't need, assume...
+        }
+
+        // Class 1 combo box changing
+        private void class1Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (class1Menu.SelectedIndex)
+            {
+                case -1: 
+                case 0:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("CptS 423",new ArrayList()))]); break;
+                case 1:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("CptS 423", new ArrayList()))]);
+                    AddSection(new Class("CptS 423",new ArrayList()), new Section(13,1,false)); break;
+                case 2:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("CptS 423", new ArrayList()))]);
+                    AddSection(new Class("CptS 423", new ArrayList()), new Section(14, 2, false)); break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        // Class 2 combo box changing
+        private void class2Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (class2Menu.SelectedIndex)
+            {
+                case -1:
+                case 0:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("PHYS 201", new ArrayList()))]); break;
+                case 1:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("PHYS 201", new ArrayList()))]);
+                    AddSection(new Class("PHYS 201", new ArrayList()), new Section(13, 1, true)); break;
+                case 2:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("PHYS 201", new ArrayList()))]);
+                    AddSection(new Class("PHYS 201", new ArrayList()), new Section(14, 2, true)); break;
+                case 3:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("PHYS 201", new ArrayList()))]);
+                    AddSection(new Class("PHYS 201", new ArrayList()), new Section(12, 3, false)); break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        // Class 3 combo box changing
+        private void class3Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (class3Menu.SelectedIndex)
+            {
+                case -1:
+                case 0:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("MATH 172", new ArrayList()))]); break;
+                case 1:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("MATH 172", new ArrayList()))]);
+                    AddSection(new Class("MATH 172", new ArrayList()), new Section(9, 1, true)); break;
+                case 2:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("MATH 172", new ArrayList()))]);
+                    AddSection(new Class("MATH 172", new ArrayList()), new Section(13, 2, true)); break;
+                case 3:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("MATH 172", new ArrayList()))]);
+                    AddSection(new Class("MATH 172", new ArrayList()), new Section(10, 3, false)); break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        // Class 4 combo box changing
+        private void class4Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (class4Menu.SelectedIndex)
+            {
+                case -1:
+                case 0:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("MATH 216", new ArrayList()))]); break;
+                case 1:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("MATH 216", new ArrayList()))]);
+                    AddSection(new Class("MATH 216", new ArrayList()), new Section(9, 1, true)); break;
+                case 2:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("MATH 216", new ArrayList()))]);
+                    AddSection(new Class("MATH 216", new ArrayList()), new Section(14, 2, true)); break;
+                case 3:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("MATH 216", new ArrayList()))]);
+                    AddSection(new Class("MATH 216", new ArrayList()), new Section(8, 3, true)); break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        // Class 5 combo box changing
+        private void class5Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (class5Menu.SelectedIndex)
+            {
+                case -1:
+                case 0:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("CptS 260", new ArrayList()))]); break;
+                case 1:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("CptS 260", new ArrayList()))]);
+                    AddSection(new Class("CptS 260", new ArrayList()), new Section(10, 1, true)); break;
+                case 2:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("CptS 260", new ArrayList()))]);
+                    AddSection(new Class("CptS 260", new ArrayList()), new Section(12, 2, true)); break;
+                case 3:
+                    RemoveClass((Class)listOfClasses[listOfClasses.LastIndexOf(new Class("CptS 260", new ArrayList()))]);
+                    AddSection(new Class("CptS 260", new ArrayList()), new Section(11, 3, false)); break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Finish_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
